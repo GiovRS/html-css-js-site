9dfa6d314b1144e5165849bc3e504d24ba9fb1d2
GiovRS <97317881+GiovRS@users.noreply.github.com>
2024-12-13 18:27:32 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,18 @@
+'use strict'
 
+
+const switcher = document.querySeletor('.btn');
+
+switcher.addEventListener('click', function() {
+  document.body.classList.toggle('dark-theme')
+
+var ClassName = document.body.className;
+if (className == light-theme") {
+  this.text.Content = "Dark";
+}
+else {
+  this.text.Content = "Light";
+}
+
+console.log('current class name: ' + className);
+});
